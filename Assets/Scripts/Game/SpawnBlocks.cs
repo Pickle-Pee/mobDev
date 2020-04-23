ae37a3450661971f9cfd462846250f7f973cc8d9
Pickle-Pee <kirillkas5642013@gmail.com>
2020-04-23 14:53:33 +0300

    initial commit

---
+++ Assets/Scripts/Game/SpawnBlocks.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlocks : MonoBehaviour {
+    
+    public GameObject block;
+    private GameObject blockInst;
+    void Start() {
+        blockInst = Instantiate (block, new Vector3 (Random.Range (9f, 19f), Random.Range (3f, 5f), 0f), Quaternion.identity) as GameObject;
+    }
+}
+
---
+++ Assets/Scripts/MainScene/GameArangement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameArangement : MonoBehaviour {
+
+    public Text playTxt, gameName;
+
+    private bool clicked;
+
+    void OnMouseDown() {
+        if (!clicked) {
+            clicked = true;
+            playTxt.gameObject.SetActive (false);
+            gameName.text = "0";
+        }
+
+        
+    }
+}
